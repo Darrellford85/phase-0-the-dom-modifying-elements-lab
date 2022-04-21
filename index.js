9d27a9710910e736b4c7b196719b61b69b58b912
Darrell Ford <Darrellford85@gmail.com>
2022-04-21 06:02:14 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const element = document.getElementById("main");
+// queryslector does not complete task as seen in assessing element video, will try and use element.remove() and element.create() to remove and create as newheader is failing//
+element.remove();
+
+const newHeader = document.createElement("h1");
+//^//
+
+
+newHeader.id = "victory";
+newHeader.innerHTML = "DarrellSubmit is the champion"
